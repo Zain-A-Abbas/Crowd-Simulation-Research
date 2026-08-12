5a63c68b3b951b3821f783c0fdb7a97f83f4a073
Zain Abbas <zainaabbas347@gmail.com>
2026-08-12 20:02:58 +0500

    Zoom feature

--- Simulation/camera_2d.gd
+++ Simulation/camera_2d.gd
@@ -4,15 +4,20 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.is_pressed():
 			if event.keycode == KEY_Z:
-				zoom += Vector2(0.05, 0.05)
+				zoom_in()
 			elif event.keycode == KEY_X && zoom - Vector2(0.1, 0.1) > Vector2(0.01, 0.01):
-				zoom -= Vector2(0.05, 0.05)
+				zoom_out()
 	
 	if event is InputEventMouseButton:
 		if event.button_index == 4:
-			zoom += Vector2(0.05, 0.05)
+			zoom_in(0.05)
 		elif event.button_index == 5 && zoom - Vector2(0.1, 0.1) > Vector2(0.01, 0.01):
-			zoom -= Vector2(0.05, 0.05)
+			zoom_out(0.05)
 	if event is InputEventMouseMotion:
 		if event.pressure:
 			offset -= event.relative / zoom
+
+func zoom_in(amount: float = 0.1):
+	zoom += Vector2(amount, amount)
+func zoom_out(amount: float = 0.1):
+	zoom -= Vector2(amount, amount)
